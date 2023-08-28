7d4482b0c6526263d1765f64726283eeb7064357
Maycon Braga <maycondbraga.s@gmail.com>
2023-08-27 22:47:57 -0300

    feat: add typeorm

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
+  return response.status(200).json({ message: 'Hello World!' });
 });
 
 export default routes;
