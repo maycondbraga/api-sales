d3932213240390ae07518086d6c18cafab2546ba
Maycon Braga <maycondbraga.s@gmail.com>
2023-08-28 01:09:58 -0300

    feat: add migrations and first entity

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('products')
+class Product {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
